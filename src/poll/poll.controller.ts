8c9d1f5c5dacc44c8ed11a77602d2b0fd25628d5
ChrisDillinger <dillingerdevelopment@gmail.com>
2019-06-23 18:35:01 -0400

    singular typo

--- src/poll/poll.controller.ts
+++ src/poll/poll.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 
 @Controller('polls')
-export class PollsController {
+export class PollController {
   @Get()
   findAll(): string {
     return 'This action returns all polls';
